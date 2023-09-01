0cab168c49808fb159f7388fa5fc6d73411e1bed
Kaylee Knox <kjknoxx90@gmail.com>
2023-09-01 09:20:44 -0500

    finished quiz 2, working on some code for DOM practice.

---
+++ js/DOM-manuipulation-loops-arrays-objects.js
@@ -0,0 +1,19 @@
+// TODO: Reference your past code or write fresh code to make a book object. This object should have properties to store the book's title, the book's author, and (optionally) the genre or another property that would make sense for the object to have.
+
+//TODO: Uncomment the following variable - fill in the string interpolation expressions to access the properties of your book to finish the following card.
+
+// let myCard = `<div class="card">
+//     <div>Book Title: ${obj.prop}.</div>
+//     <div>Book Author: ${obj.prop}.</div>
+//     <div>Book Genre: ${obj.prop}.</div>
+// </div>`
+
+//TODO: Using JavaScript, add your finished HTML card to the DOM so our users will be able to see it on page load. We want target the innerHTML of #container.
+
+
+//TODO: From prior work or fresh in this file, let's make an array of book objects (at least three).
+
+
+//TODO: Refactor your above code - our HTML needs to build three HTML cards with the information from the objects. Hint: We would want to iterate through the array and add each fresh bit of HTML into the variable of allCards set up below.
+
+let allCards = ""; //bucket to store the HTML you build in the loop
